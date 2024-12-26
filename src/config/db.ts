fefeb50039a0ae3dc9a8edff63b46a9d8b39c2c8
shashank-nimbly <shashank@hellonimbly.com>
2024-12-27 00:26:42 +0530

    dev env setup

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,14 +8,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//Container
+// const sequelize = new Sequelize ( {
+//   database: process.env.DATABASE_NAME || "music_library",
+//   dialect: "postgres",
+//   username: process.env.DATABASE_USER || "user",
+//   password: process.env.DATABASE_PASSWORD || "password",
+//   host: process.env.DATABASE_HOST || "db",
+//   models: [User, Artist, Album, Track, Favorite], // Add all models here
+//   logging: false,
+// });
+//Development
 const sequelize = new Sequelize({
-  database: process.env.DATABASE_NAME || "music_library",
+  database: "music_library",
   dialect: "postgres",
-  username: process.env.DATABASE_USER || "user",
-  password: process.env.DATABASE_PASSWORD || "password",
-  host: process.env.DATABASE_HOST || "db",
-  models: [User, Artist, Album, Track, Favorite], // Add all models here
-  logging: false,
+  username: "shashank",
+  password: "shashank",
+  host: "localhost",
+  port: 5433,
+  models: [User, Artist, Album, Track, Favorite],
+  logging: false
 });
+
 
 export default sequelize;
